2e0071b68b4b74b327a81c55a0b87dbdee638159
JoséDavid <JosDavid@172.23.25.151>
2014-11-01 19:49:59 -0600

    - Creación paquetes usuario e items.

---
+++ src/dogLovers/vista/items/package-info.java
@@ -0,0 +1,21 @@
+/**
+ * Proyecto Final POO
+ * Jose David Mora Loria
+ * 2014004856
+ * Diego Delgado Cerdas
+ * 2013099268
+ * David Diaz
+ * 2014004725
+ * Roger Villalobos
+ * 2014079369
+ * 01/11/2014
+ */
+
+/**
+ * @author JoséDavid 01/11/2014
+ */
+package dogLovers.vista.items;
+
+/*
+ * Contenido del paquete: Paneles bases para cada item de las diferentes listas.
+ */
---
+++ src/dogLovers/vista/usuario/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Proyecto Final POO
+ * Jose David Mora Loria
+ * 2014004856
+ * Diego Delgado Cerdas
+ * 2013099268
+ * David Diaz
+ * 2014004725
+ * Roger Villalobos
+ * 2014079369
+ * 01/11/2014
+ */
+
+/**
+ * @author JoséDavid 01/11/2014
+ */
+package dogLovers.vista.usuario;
+
+/*
+ * Contenido del paquete: Todas las clases y métodos referentes al diseño e
+ * implementación gráfica de las ventanas propias del usuario.
+ */
