9f437a6a43f1100c778d3b3eaa86e5613df591b5
rgvillalobosr <otakuroger@hotmail.com>
2014-11-09 22:15:00 -0600

    - Creacion de CrearUsuario.

--- src/dogLovers/vista/usuario/CrearUsuario.java
+++ src/dogLovers/vista/usuario/CrearUsuario.java
@@ -15,6 +15,7 @@ package dogLovers.vista.usuario;
 import java.awt.*;
 
 import dogLovers.control.Principal;
+import dogLovers.modelo.Usuario;
 import dogLovers.vista.*;
 
 import javax.swing.JPanel;
@@ -22,6 +23,7 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 
+import java.awt.event.*;
 /**
  * @author Róger 9/11/2014
  */
@@ -29,20 +31,22 @@ public class CrearUsuario extends VentanaBase {
 
 
 	private static final long serialVersionUID = -4636032409756183301L;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
-	private JLabel lblRegistrar;
-	private JLabel lblIdentificacion;
-	private JLabel lblNombre;
-	private JLabel lblApellido;
-	private JLabel lblApellido_1;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField txtApellido2_1;
+	private JTextField txtApellido1_1;
+	private JTextField txtNombre_1;
+	private JTextField txtCedula;
+	private JTextField txtTelefono;
+	private JTextField txtCorreo;
+	private JTextField txtUbicacion;
+	private JLabel lblCedula;
+	private JLabel lblNombre_1;
+	private JLabel lblApellido1_1;
+	private JLabel lblApellido2_1;
+	private JLabel lblUbicacion;
+	private JLabel lblCorreo;
+	private Usuario usuario;
 
-	public CrearUsuario() {
+	public CrearUsuario( ) {
 		PanelBase panel = new PanelBase();
 		setContentPane(panel);
 		GridBagLayout gbl_panel = new GridBagLayout();
@@ -51,7 +55,7 @@ public class CrearUsuario extends VentanaBase {
 		gbl_panel.columnWeights = new double[]{0.0, 1.0, 0.0};
 		gbl_panel.rowWeights = new double[]{0.0, 1.0, 0.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
-		
+
 		JPanel panelRegistro = new JPanel();
 		panelRegistro.setOpaque(false);
 		GridBagConstraints gbc_panel = new GridBagConstraints();
@@ -66,7 +70,7 @@ public class CrearUsuario extends VentanaBase {
 		gbl_panelRegistro.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.2, 1.0};
 		gbl_panelRegistro.rowWeights = new double[]{ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 		panelRegistro.setLayout(gbl_panelRegistro);
-		
+
 		JLabel lblRegistrar_1 = new JLabel("Registrar");
 		lblRegistrar_1.setFont(Principal.getLetratexto1());
 		GridBagConstraints gbc_lblRegistrar_1 = new GridBagConstraints();
@@ -75,7 +79,7 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblRegistrar_1.gridx = 3;
 		gbc_lblRegistrar_1.gridy = 0;
 		panelRegistro.add(lblRegistrar_1, gbc_lblRegistrar_1);
-		
+
 		JLabel lblLosCampos = new JLabel("Los campos");
 		lblLosCampos.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblLosCampos = new GridBagConstraints();
@@ -84,7 +88,7 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblLosCampos.gridx = 1;
 		gbc_lblLosCampos.gridy = 1;
 		panelRegistro.add(lblLosCampos, gbc_lblLosCampos);
-		
+
 		JLabel lblConAsterisco = new JLabel("con asterisco (*)");
 		lblConAsterisco.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblConAsterisco = new GridBagConstraints();
@@ -93,7 +97,7 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblConAsterisco.gridx = 2;
 		gbc_lblConAsterisco.gridy = 1;
 		panelRegistro.add(lblConAsterisco, gbc_lblConAsterisco);
-		
+
 		JLabel lblSonRequeridos = new JLabel(" son requeridos.");
 		lblSonRequeridos.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblSonRequeridos = new GridBagConstraints();
@@ -102,8 +106,8 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblSonRequeridos.gridx = 3;
 		gbc_lblSonRequeridos.gridy = 1;
 		panelRegistro.add(lblSonRequeridos, gbc_lblSonRequeridos);
-		
-		JLabel lblCedula = new JLabel("C\u00E9dula: *");
+
+		lblCedula = new JLabel("C\u00E9dula: *");
 		lblCedula.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblCedula = new GridBagConstraints();
 		gbc_lblCedula.insets = new Insets(0, 0, 5, 5);
@@ -111,35 +115,37 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblCedula.gridx = 1;
 		gbc_lblCedula.gridy = 3;
 		panelRegistro.add(lblCedula, gbc_lblCedula);
-		
-		textField_3 = new JTextField();
-		GridBagConstraints gbc_textField_3 = new GridBagConstraints();
-		gbc_textField_3.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_3.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_3.gridx = 2;
-		gbc_textField_3.gridy = 3;
-		panelRegistro.add(textField_3, gbc_textField_3);
-		textField_3.setColumns(10);
-		
-		JLabel lblApellido1 = new JLabel("1\u00BA Apellido: *");
-		lblApellido1.setFont(Principal.getLetratexto3());
-		GridBagConstraints gbc_lblApellido = new GridBagConstraints();
-		gbc_lblApellido.insets = new Insets(0, 0, 5, 5);
-		gbc_lblApellido.anchor = GridBagConstraints.WEST;
-		gbc_lblApellido.gridx = 4;
-		gbc_lblApellido.gridy = 3;
-		panelRegistro.add(lblApellido1, gbc_lblApellido);
-		
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 5;
-		gbc_textField_1.gridy = 3;
-		panelRegistro.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblNombre_1 = new JLabel("Nombre: *");
+
+		txtCedula = new JTextField();
+		soloNumeros(txtCedula);
+		GridBagConstraints gbc_txtCedula = new GridBagConstraints();
+		gbc_txtCedula.insets = new Insets(0, 0, 5, 5);
+		gbc_txtCedula.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtCedula.gridx = 2;
+		gbc_txtCedula.gridy = 3;
+		panelRegistro.add(txtCedula, gbc_txtCedula);
+		txtCedula.setColumns(10);
+
+		lblApellido1_1 = new JLabel("1\u00BA Apellido: *");
+		lblApellido1_1.setFont(Principal.getLetratexto3());
+		GridBagConstraints gbc_lblApellido1_1 = new GridBagConstraints();
+		gbc_lblApellido1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblApellido1_1.anchor = GridBagConstraints.WEST;
+		gbc_lblApellido1_1.gridx = 4;
+		gbc_lblApellido1_1.gridy = 3;
+		panelRegistro.add(lblApellido1_1, gbc_lblApellido1_1);
+
+		txtApellido1_1 = new JTextField();
+		soloLetras(txtApellido1_1);
+		GridBagConstraints gbc_txtApellido1_1 = new GridBagConstraints();
+		gbc_txtApellido1_1.insets = new Insets(0, 0, 5, 5);
+		gbc_txtApellido1_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtApellido1_1.gridx = 5;
+		gbc_txtApellido1_1.gridy = 3;
+		panelRegistro.add(txtApellido1_1, gbc_txtApellido1_1);
+		txtApellido1_1.setColumns(10);
+
+		lblNombre_1 = new JLabel("Nombre: *");
 		GridBagConstraints gbc_lblNombre_1 = new GridBagConstraints();
 		lblNombre_1.setFont(Principal.getLetratexto3());
 		gbc_lblNombre_1.insets = new Insets(0, 0, 5, 5);
@@ -147,34 +153,36 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblNombre_1.gridx = 1;
 		gbc_lblNombre_1.gridy = 4;
 		panelRegistro.add(lblNombre_1, gbc_lblNombre_1);
-		
-		textField_2 = new JTextField();
-		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_2.gridx = 2;
-		gbc_textField_2.gridy = 4;
-		panelRegistro.add(textField_2, gbc_textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblApellido2 = new JLabel("2\u00BA Apellido: *");
-		lblApellido2.setFont(Principal.getLetratexto3());
-		GridBagConstraints gbc_lblApellido2 = new GridBagConstraints();
-		gbc_lblApellido2.insets = new Insets(0, 0, 5, 5);
-		gbc_lblApellido2.anchor = GridBagConstraints.WEST;
-		gbc_lblApellido2.gridx = 4;
-		gbc_lblApellido2.gridy = 4;
-		panelRegistro.add(lblApellido2, gbc_lblApellido2);
-		
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 5;
-		gbc_textField.gridy = 4;
-		panelRegistro.add(textField, gbc_textField);
-		textField.setColumns(10);
-		
+
+		txtNombre_1 = new JTextField();
+		soloLetras(txtNombre_1);
+		GridBagConstraints gbc_txtNombre_1 = new GridBagConstraints();
+		gbc_txtNombre_1.insets = new Insets(0, 0, 5, 5);
+		gbc_txtNombre_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtNombre_1.gridx = 2;
+		gbc_txtNombre_1.gridy = 4;
+		panelRegistro.add(txtNombre_1, gbc_txtNombre_1);
+		txtNombre_1.setColumns(10);
+
+		lblApellido2_1 = new JLabel("2\u00BA Apellido: *");
+		lblApellido2_1.setFont(Principal.getLetratexto3());
+		GridBagConstraints gbc_lblApellido2_1 = new GridBagConstraints();
+		gbc_lblApellido2_1.insets = new Insets(0, 0, 5, 5);
+		gbc_lblApellido2_1.anchor = GridBagConstraints.WEST;
+		gbc_lblApellido2_1.gridx = 4;
+		gbc_lblApellido2_1.gridy = 4;
+		panelRegistro.add(lblApellido2_1, gbc_lblApellido2_1);
+
+		txtApellido2_1 = new JTextField();
+		soloLetras(txtApellido2_1);
+		GridBagConstraints gbc_txtApellido2_1 = new GridBagConstraints();
+		gbc_txtApellido2_1.insets = new Insets(0, 0, 5, 5);
+		gbc_txtApellido2_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtApellido2_1.gridx = 5;
+		gbc_txtApellido2_1.gridy = 4;
+		panelRegistro.add(txtApellido2_1, gbc_txtApellido2_1);
+		txtApellido2_1.setColumns(10);
+
 		JLabel lblTelefono = new JLabel("Tel\u00E9fono: *");
 		lblTelefono.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblTelefono = new GridBagConstraints();
@@ -183,17 +191,18 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblTelefono.gridx = 1;
 		gbc_lblTelefono.gridy = 5;
 		panelRegistro.add(lblTelefono, gbc_lblTelefono);
-		
-		textField_4 = new JTextField();
-		GridBagConstraints gbc_textField_4 = new GridBagConstraints();
-		gbc_textField_4.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_4.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_4.gridx = 2;
-		gbc_textField_4.gridy = 5;
-		panelRegistro.add(textField_4, gbc_textField_4);
-		textField_4.setColumns(10);
-		
-		JLabel lblCorreo = new JLabel("Correo: *");
+
+		txtTelefono = new JTextField();
+		soloNumeros(txtTelefono);
+		GridBagConstraints gbc_txtTelefono = new GridBagConstraints();
+		gbc_txtTelefono.insets = new Insets(0, 0, 5, 5);
+		gbc_txtTelefono.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtTelefono.gridx = 2;
+		gbc_txtTelefono.gridy = 5;
+		panelRegistro.add(txtTelefono, gbc_txtTelefono);
+		txtTelefono.setColumns(10);
+
+		lblCorreo = new JLabel("Correo: *");
 		lblCorreo.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblCorreo = new GridBagConstraints();
 		gbc_lblCorreo.anchor = GridBagConstraints.WEST;
@@ -201,17 +210,17 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblCorreo.gridx = 4;
 		gbc_lblCorreo.gridy = 5;
 		panelRegistro.add(lblCorreo, gbc_lblCorreo);
-		
-		textField_5 = new JTextField();
-		GridBagConstraints gbc_textField_5 = new GridBagConstraints();
-		gbc_textField_5.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_5.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_5.gridx = 5;
-		gbc_textField_5.gridy = 5;
-		panelRegistro.add(textField_5, gbc_textField_5);
-		textField_5.setColumns(10);
-		
-		JLabel lblUbicacion = new JLabel("Ubicaci\u00F3n:");
+
+		txtCorreo = new JTextField();
+		GridBagConstraints gbc_txtCorreo = new GridBagConstraints();
+		gbc_txtCorreo.insets = new Insets(0, 0, 5, 5);
+		gbc_txtCorreo.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtCorreo.gridx = 5;
+		gbc_txtCorreo.gridy = 5;
+		panelRegistro.add(txtCorreo, gbc_txtCorreo);
+		txtCorreo.setColumns(10);
+
+		lblUbicacion = new JLabel("Ubicaci\u00F3n:");
 		lblUbicacion.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_lblUbicacion = new GridBagConstraints();
 		gbc_lblUbicacion.anchor = GridBagConstraints.WEST;
@@ -219,16 +228,16 @@ public class CrearUsuario extends VentanaBase {
 		gbc_lblUbicacion.gridx = 1;
 		gbc_lblUbicacion.gridy = 6;
 		panelRegistro.add(lblUbicacion, gbc_lblUbicacion);
-		
-		textField_6 = new JTextField();
-		GridBagConstraints gbc_textField_6 = new GridBagConstraints();
-		gbc_textField_6.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_6.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_6.gridx = 2;
-		gbc_textField_6.gridy = 6;
-		panelRegistro.add(textField_6, gbc_textField_6);
-		textField_6.setColumns(10);
-		
+
+		txtUbicacion = new JTextField();
+		GridBagConstraints gbc_txtUbicacion = new GridBagConstraints();
+		gbc_txtUbicacion.insets = new Insets(0, 0, 5, 5);
+		gbc_txtUbicacion.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtUbicacion.gridx = 2;
+		gbc_txtUbicacion.gridy = 6;
+		panelRegistro.add(txtUbicacion, gbc_txtUbicacion);
+		txtUbicacion.setColumns(10);
+
 		JButton btnGuardar = new JButton("Guardar");
 		btnGuardar.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_btnGuardar = new GridBagConstraints();
@@ -236,15 +245,53 @@ public class CrearUsuario extends VentanaBase {
 		gbc_btnGuardar.gridx = 4;
 		gbc_btnGuardar.gridy = 7;
 		panelRegistro.add(btnGuardar, gbc_btnGuardar);
-		
+
 		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent arg0) {
+				dogLovers.vista.Login regresar = new dogLovers.vista.Login();
+				regresar.setVisible(true);
+				setVisible(false);
+			}
+		});
 		btnCancelar.setFont(Principal.getLetratexto3());
 		GridBagConstraints gbc_btnCancelar = new GridBagConstraints();
 		gbc_btnCancelar.insets = new Insets(0, 0, 5, 5);
 		gbc_btnCancelar.gridx = 5;
 		gbc_btnCancelar.gridy = 7;
 		panelRegistro.add(btnCancelar, gbc_btnCancelar);
-		
+
 	}
 
+
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+
+
+	public void soloLetras(JTextField txt){
+		txt.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c= e.getKeyChar();
+				if (Character.isDigit(c)) {
+					e.consume();		
+				}
+			}
+		});
+	}
+
+	public void soloNumeros(JTextField txt){
+		txt.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				char c= e.getKeyChar();
+				if (!Character.isDigit(c)) {
+					e.consume();		
+				}
+			}
+		});
+	}
 }
